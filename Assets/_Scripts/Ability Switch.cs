d62ffb57d43b7b37859cab1a36bc2629d44ce98d
simon4100 <4100simon@gmail.com>
2024-09-16 12:32:53 +0200

    Ability Switch

---
+++ Assets/_Scripts/Ability Switch.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class AbilitySwitch : MonoBehaviour
+{
+    public int selectedAbility = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectAbility();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int previousSelectedAbility = selectedAbility;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedAbility >= transform.childCount -1) { 
+                selectedAbility = 0;
+            }
+            else
+            {
+                selectedAbility++;
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedAbility <= 0)
+                selectedAbility = transform.childCount - 1;
+            else
+                selectedAbility--;
+
+        if (previousSelectedAbility != selectedAbility)
+            {
+                selectAbility();
+            }
+
+        }
+    }
+
+    void selectAbility()
+    {
+        int i = 0;
+        foreach (Transform ability in transform)
+        {
+            if (i == selectedAbility)
+                ability.gameObject.SetActive(true);
+            else
+                ability.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}
