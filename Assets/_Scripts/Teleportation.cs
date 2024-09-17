f44e183c3150fa0199471ecfa5bea424b0ed3571
shamblez-44 <engslevbenjamin@gmail.com>
2024-09-17 17:26:37 +0200

    Cooldown

--- Assets/_Scripts/Teleportation.cs
+++ Assets/_Scripts/Teleportation.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Teleportation : MonoBehaviour
 {
+    public float cooldown = 1f;
+    private float timeToShoot = 0;
+
     public Transform player;
     // Start is called before the first frame update
     void Start()
@@ -14,13 +17,15 @@ public class Teleportation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && timeToShoot <= 0)
         {
             Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             target.z = 0;
             player.position = target; // teleporting
-            
+
+            timeToShoot = cooldown;
         }
-    }
-        
+
+        timeToShoot -= Time.deltaTime;
+    }     
 }
